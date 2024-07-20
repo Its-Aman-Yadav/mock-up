dd2650237a99bd47ae21caa58cee98251747eaa1
Aman Yadav <itsamanyadav18@gmail.com>
2024-07-20 07:46:58 +0530

    Authentication Completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <>
-   <h1 className="flex justify-center ">Hello World</h1>
+   <h1 className="flex justify-center mt-10">Hello World</h1>
    </>
   );
 }
